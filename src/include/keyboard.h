ab5a04e406eb9c59a1d786a704330937f3cf8c52
gamma63 <174414990+gamma63@users.noreply.github.com>
2024-07-27 20:36:34 +0300

    Update keyboard.h

--- src/include/keyboard.h
+++ src/include/keyboard.h
@@ -1,5 +1,33 @@
 #ifndef KEYBOARD_H
 
 #define kbver "1.0" // Driver version
+
+#pragma once
+
+#include <stdint.h>
+
+
+
+#define MAX_STRING_SIZE 100
+#define MAX_HISTORY_LENGTH 20
+
+struct Key {
+    char Char;
+    char ShiftedChar;
+    uint8_t Code;
+};
+
+
+extern char command_history [MAX_HISTORY_LENGTH][MAX_STRING_SIZE];
+extern int history_length;
+
+char* scanstring();
+char* scancmd();
+uint8_t inb(uint16_t port);
+int is_keyboard_data_available();
+uint8_t get_scancode();
+struct Key get_char_from_code(uint8_t code);
+struct Key scanchar();
+char* scanstring();
  
 #endif // KEYBOARD_H
