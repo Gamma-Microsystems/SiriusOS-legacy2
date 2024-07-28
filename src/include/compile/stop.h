3b26847d47e0c5411cc62f935ab388d714aee190
gamma63 <174414990+gamma63@users.noreply.github.com>
2024-07-28 15:34:52 +0300

    Update stop.h

--- src/include/compile/stop.h
+++ src/include/compile/stop.h
@@ -5,9 +5,9 @@
 #error "You are not using a cross-compiler, you will most certainly run into trouble"
 #endif
 
-/* This tutorial will only work for the 32-bit ix86 targets. */
+/* This OS will only work for the 32-bit ix86 targets. */
 #if !defined(__i386__)
-#error "This tutorial needs to be compiled with a ix86-elf compiler"
+#error "This OS needs to be compiled with a ix86-elf compiler"
 #endif
 
 #endif // STOP_H
